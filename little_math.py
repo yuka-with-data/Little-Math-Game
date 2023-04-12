24b707ef922a252a7c3dd84ec7225204d69035c3
yuka_with_data <90426808+yuka-with-data@users.noreply.github.com>
2023-04-12 14:06:56 -0700

    Add Class and decorators

--- little_math.py
+++ little_math.py
@@ -12,82 +12,100 @@ The program should ultimately output the user’s score: the number of correct a
 
 What I am working on next:
 
-Class:
-level
-operator
-X
-Y
-
-decorators
-set X is grater than Y
-set decimal place, 2
-
 Error Handlings
+Decorators
 
  """
- # import libraries
+# import libraries
 import random
 from operator import add, sub, mul, truediv
 
-# let the user set the level 1-5
-def get_level() -> int:
-    while True:
-        try:
-            level = int(input("Level: "))
-            if level in (1,2,3,4,5):
+class MathGame:
+    def __init__(self, level=1):
+        self.level = level
+        self.op_symbol = ""
+        self.op_func = None
+        """ 
+         op_symbol and op_func are initialized to empty string and 'None'
+         because they are not known yet until the user provides valid input.
+           """
+
+    # handle errors - LEVEL
+    @property
+    def level(self):
+        return self._level
+
+    @level.setter
+    def level(self, level):
+        if level not in (1,2,3):
+            raise ValueError
+        self._level = level
+
+    @staticmethod
+    def get_level() -> int:
+        while True:
+            try:
+                level = int(input("Level: "))
+                if level not in (1,2,3):
+                    raise ValueError
                 return level
-            else:
-                raise ValueError
-        except ValueError:
-            continue
-
-# let user pick order of operation
-def get_operator() -> tuple[str, callable]:
-    operator_mapping = {
-        "+": add,
-        "-": sub,
-        "*": mul,
-        "/": truediv
-    }
-    while True:
-        operator = input("Operator: ")
-        if operator in operator_mapping:
-            return operator, operator_mapping[operator]
-        else:
-            continue
-
-def generate_int(level: int) -> int:
-    if level == 1:
-        return random.randrange(10**(level-1)-1,10**level)
-    else:
-        return random.randrange(10**(level-1),10**level)
-
-def math_prob(level: int, op_symbol: str, op_func: callable):
-    # score tracking    
-    score = 0
-    # ask user 5 unique question
-    for _ in range(5):
-        X, Y = generate_int(level), generate_int(level)
-        # user gets 3 chances to answer correctly
-        for _ in range(3):
-            ans_input = input(f"{X} {op_symbol} {Y} = ")
-            if not ans_input.isdigit():
-                print("Try Again.")
+            except ValueError as e:
+                print(e,"Invalid level. Try again.")
                 continue
-            if int(ans_input) == op_func(X,Y):
-                score += 1
-                break
+    
+    @staticmethod
+    def get_operator() -> tuple[str, callable]:
+        operator_mapping = {
+            "+": add,
+            "-": sub,
+            "*": mul,
+            "/": truediv
+        }
+        while True:
+            operator = input("Operator: ")
+            if operator in operator_mapping:
+                return operator, operator_mapping[operator]
             else:
-                print("wrong answer... Try again.")
+                print("Invalid operator. Try again.")
+                continue
+    
+    def generate_int(self) -> int:
+        if self.level == 1:
+            return random.randrange(10**(self.level-1)-1,10**self.level)
         else:
-            print(f"The answer is {op_func(X,Y)}")
-    return score
+            return random.randrange(10**(self.level-1),10**self.level)
 
+    def math_prob(self):
+        # score tracking    
+        score = 0
+        # get operator
+        self.op_symbol, self.op_func = self.get_operator()
+        # ask user 5 unique question
+        for _ in range(5):
+            X, Y = self.generate_int(), self.generate_int()
+            # Set X is always grater than Y
+            if X < Y:
+                X, Y = Y, X
+            # user gets 3 chances to answer correctly
+            for _ in range(3):
+                ans_input = input(f"{X} {self.op_symbol} {Y} = ")
+                if not ans_input.isdigit():
+                    print("Try Again.")
+                    continue
+                if int(ans_input) == self.op_func(X,Y):
+                    score += 1
+                    break
+                else:
+                    print("wrong answer... Try again.")
+            else:
+                print(f"The answer is {self.op_func(X,Y)}")
+        return score
+
+# main function
 def main():
-    #level = get_level()
-    operator_symb, operator_func = get_operator()
-    score = math_prob(get_level(), operator_symb, operator_func)
-    print(f"Your score is {score}/5. Great Job!")
+    level = MathGame.get_level()
+    game = MathGame(level=level)
+    print(f'Final Score is {game.math_prob()}')
 
 if __name__ == "__main__":
     main()
